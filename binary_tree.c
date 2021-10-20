023f0827f4cec70704bfda779fa5ad4d5d091a39
unknown <gabrieldolzan@icloud.com>
2021-10-20 18:20:07 -0300

    Added logic to support multiple digit numbers

--- binary_tree.c
+++ binary_tree.c
@@ -1,9 +1,33 @@
 #include "binary_tree.h"
 
-t_node* initialize(char value)
+t_node* initialize(char* value, int value_size)
 {
+    int symbols[SYMBOLS_SIZE] = {'*', '/', '+', '-'};
+    int symbol_flag = 0;
+    int number = 0;
+
+    if(value_size == 1)
+    {
+        for(int i = 0; i < SYMBOLS_SIZE; i++)
+        {
+            if(value[0] == symbols[i])
+                symbol_flag = 1;
+        }    
+    }
+    else
+        number = atoi(value);
+
     t_node* node = (t_node*)malloc(sizeof(t_node));
-    node->value = value;
+    if(symbol_flag == 1)
+    {
+        node->value = value;
+        node->number = NULL;
+    }
+    else
+    {
+        node->number = number;
+        node->value = NULL;
+    }
     node->right = NULL;
     node->left = NULL;
     return node;
@@ -11,11 +35,32 @@ t_node* initialize(char value)
 
 t_node* assemble_tree(char* str, int* i)
 {
+    int number_position = 0;
+    int number_size = 0;
+    int str_position = 0;
+    char* number_in_str;
     t_node* node = NULL;
 
-    if (str[*i] == '(') {
+    if (str[*i] == '(') 
+    {
         (*i)++;
-        node = initialize(str[*i]);    
+        number_position = (*i);
+
+        while(str[number_position] != ')')
+            number_position++;
+
+        number_size = (number_position - (*i)) + 1;
+        number_in_str = (char*)malloc((sizeof(char) * number_size) + 1);
+
+        for(int j = (*i); j <= number_position; j++)
+        {
+            number_in_str[str_position] = str[j];
+            str_position++;
+        }
+        node = initialize(number_in_str, number_size); 
+        (*i) = number_position;
+        free(number_in_str);
+
         (*i)++;
         node->left = assemble_tree(str, i);
         node->right = assemble_tree(str, i);            
--- binary_tree.h
+++ binary_tree.h
@@ -3,14 +3,17 @@
 
 #include <stdlib.h>
 
+#define SYMBOLS_SIZE 4
+
 typedef struct t_node
 {
     char value;
+    int number;
     struct t_node *left;
     struct t_node *right;
 } t_node;
 
-t_node* initialize(char value);
+t_node* initialize(char* value, int value_size);
 t_node* assemble_tree(char* str, int* i);
 void free_nodes(t_node* node);
 
