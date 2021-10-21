45ed169588d00a24a3ea8dee28a7a6d6d6fbdbcd
gpdolzan <gabrieldolzan@icloud.com>
2021-10-20 22:56:06 -0300

    Adicionei alguns printfs para deixar o programa mais agradavel

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
 
 int main()
 {
-    //Pega string do stdin, sendo essa string a expressao aritmetica
     char str[MAX_STR_LEN];
+
+    //Pega string do stdin, sendo essa string a expressao aritmetica
+    fprintf(stderr, "Por favor, insira a expressao que deseja calcular: ");
     scanf("%s[^\n]", str);
+    fprintf(stderr, "Mostrando contas realizadas:\n");
 
     //Constroi uma arvore para analisar a expressao aritmetica
     start_program(str);
